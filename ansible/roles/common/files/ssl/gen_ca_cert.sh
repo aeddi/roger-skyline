709000476ddb1b656bfb02c27c642a92c51838a2
ansible user <ansible@Source>
2016-10-14 12:59:20 +0200

    10/14: misc ; WIP mail ; refactored LDAP role a bit.

--- ansible/roles/common/files/ssl/gen_ca_cert.sh
+++ ansible/roles/common/files/ssl/gen_ca_cert.sh
@@ -1,2 +1,2 @@
-certtool -p --outfile ca_certs.key
-certtool -s --load-privkey ca_certs.key --template ca_certs.conf --outfile ca_server.pem
+certtool -p --outfile ca_cert.key
+certtool -s --load-privkey ca_cert.key --template ca_cert.conf --outfile ca_server.pem
--- ansible/roles/common/files/ssl/gen_ca_certs.sh
+++
@@ -1,2 +0,0 @@
-certtool -p --outfile ca_certs.key
-certtool -s --load-privkey ca_certs.key --template ca_certs.conf --outfile ca_server.pem
