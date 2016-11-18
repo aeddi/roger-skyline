220ac1a97560addf3b75ee1ed48deddf5e5c426f
ansible user <ansible@Source>
2016-11-18 19:31:08 +0100

    XMPP: chatrooms work!

--- ansible/roles/xmpp/files/xmpp.slash16.local.cfg.lua
+++ ansible/roles/xmpp/files/xmpp.slash16.local.cfg.lua
@@ -11,14 +11,15 @@ VirtualHost "xmpp.slash16.local"
 		certificate = "/etc/ssl/certs/cert.pem";
     ciphers = "EECDH+ECDSA+AESGCM EECDH+aRSA+AESGCM EECDH+ECDSA+SHA384 EECDH+ECDSA+SHA256 EECDH+aRSA+SHA384 EECDH+aRSA+SHA256 EECDH EDH+aRSA !RC4 !aNULL !eNULL !LOW !3DES !MD5 !EXP !PSK !SRP !DSS";
 		}
-
+  admins = { "mcizo@xmpp.slash16.local" }
 ------ Components ------
 -- You can specify components to add hosts that provide special services,
 -- like multi-user conferences, and transports.
 -- For more information on components, see http://prosody.im/doc/components
 
 -- Set up a MUC (multi-user chat) room server on conference.example.com:
---Component "conference.example.com" "muc"
+Component "conference.10.17.1.154" "muc"
+  name = "Slash16 chatrooms server"
 
 -- Set up a SOCKS5 bytestream proxy for server-proxied file transfers:
 --Component "proxy.example.com" "proxy65"
