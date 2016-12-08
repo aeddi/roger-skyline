bd7fb6c8d6e7818a9e9d04fd9248fe17623687f7
ansible user <ansible@Source>
2016-12-09 00:39:37 +0100

    DbSlave/Backup: Cron'd dump, rotation (+versioning: earlier time)

---
+++ ansible/initial_setup/roles/db_slave/files/cron/job_logger.source.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Source to gain syslog logging
+
+TEMP_ERR_FILE=$(mktemp)
+EXIT_TRAP=cleanup
+if readlink /proc/$$/fd/2 | grep -q pts; then
+    exec 2> >(tee $TEMP_ERR_FILE >&2)
+else
+    exec 2> $TEMP_ERR_FILE
+fi
+
+function cleanup {
+    if [ -n "$TEMP_ERR_FILE" ] && [ -f "$TEMP_ERR_FILE" ]; then
+        rm -rf "$TEMP_ERR_FILE"
+    fi
+}
+# EXIT manages any blockable sig and ERR.
+EXIT_CONDITIONS=(EXIT)
+trap "${EXIT_TRAP}" ${EXIT_CONDITIONS[@]}
+
+function this_logger {
+    local facility="$1"
+    local severity="$2"
+    shift ; shift
+    logger -t "$SYSLOG_TAG" -p "$facility.$severity" $@
+}
+
+function error {
+    local lineno=$1  
+    local msg="$2"
+    local ecode="${3:-1}"
+
+    if [ -z "$msg" ]; then
+        msg=$(cat "$TEMP_ERR_FILE")
+    fi
+    if readlink /proc/$$/fd/1 | grep -q pts; then
+      echo "Error line ${lineno} (exit ${ecode}): ${msg}"
+    fi
+    save_IFS="${IFS}"
+    this_logger cron error "Error line ${lineno} (exit ${ecode}):"
+    IFS=$'\n'
+    for l in "$msg"; do
+        this_logger cron error "${l}"
+    done
+    IFS="${save_IFS}"
+}
+
+trap 'error ${LINENO}' ERR
---
+++ ansible/initial_setup/roles/db_slave/files/cron/pg-slave-dump.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+source /opt/cronjobs/postgres/bin/job_logger.source.sh
+
+export USER=$(id -un)
+
+SYSLOG_TAG="[CRON-$USER: DB_Slave ${$}]"
+
+THIS_SCRIPT="$(realpath $0)"
+
+#From $HOME/.local/share/cron/ to $HOME/.local/bin/
+LOGROTATE_USER_WRAPPER="$(realpath $(dirname $THIS_SCRIPT)/../bin/logrotate_user_wrapper.sh)"
+LOGROTATE_USER_CONF="$(realpath $(dirname $THIS_SCRIPT)/../etc/pg_dump_logrotate.conf)"
+
+if [ "$(id -un)" != "postgres" ]; then
+  if [ "$(id -u)" != "0" ]; then
+    this_logger cron error "Not \`\`postgres'' user, neither root; exiting."
+    exit 1
+  else
+    this_logger cron notice "Run as root; becoming \`\`postgres'' user. Please run this job as user \`\`postgres'' instead."
+    exec su postgres -c "$THIS_SCRIPT"
+  fi
+fi
+
+PG_BACKUP_DIR=/var/backups/postgres
+DUMP_DIR=$PG_BACKUP_DIR
+DB_NAME=django_db
+
+if [ ! -d "$DUMP_DIR" ]; then
+  mkdir -p "$DUMP_DIR"
+fi
+
+cd "$DUMP_DIR"
+DUMP_NAME="${DB_NAME}-dump-$(date +%s).pgz"
+
+# '.pgz' ext named after the custom compressed psql format
+# (which allows discriminated restoration by table)
+/usr/bin/pg_dump -Fc "$DB_NAME" > "$DUMP_NAME"
+
+if [ ! -f "$DUMP_NAME" ]; then
+    this_logger cron error "The new dump is missing !"
+    exit 1
+fi
+
+ROTATABLE_NAME="django_db-dump.pgz"
+
+if [ -f "$ROTATABLE_NAME" ]; then
+    this_logger cron notice "Found previous dump. Force-rotating..."
+    $LOGROTATE_USER_WRAPPER --force $LOGROTATE_USER_CONF
+fi
+
+mv "$DUMP_NAME" "$ROTATABLE_NAME"
